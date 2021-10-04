7a84c2b687f97f0f29699ba1591f18dcc7760839
Bruno Beltran <brunobeltran0@gmail.com>
2021-10-04 01:38:02 -0700

    BUGFIX: support for dataclass and Enum

--- numpydoc_lint/__main__.py
+++ numpydoc_lint/__main__.py
@@ -1,15 +1,19 @@
 """Main script for numpydoc_lint."""
 import argparse
+import dataclasses
 import importlib
 import inspect
 import pkgutil
+import re
 import sys
+from enum import EnumMeta
 from functools import partial
 from pathlib import Path
 
+from loguru import logger
 from numpydoc.validate import validate
 
-from . import NumpydocReturn
+from numpydoc_lint import NumpydocReturn
 
 
 def _defined_in_class(meth, cls):
@@ -34,13 +38,29 @@ def _class_submembers_r(cls):
             yield _class_submembers_r(submember)
 
 
-def _validate_name(name, args):
+def _validate_name(name, args, is_enum):
     validate_results = validate(name)
     validate_results["errors"] = [
         (err_id, err_desc)
         for err_id, err_desc in validate_results["errors"]
         if err_id not in args.exclude_codes
     ]
+    if is_enum:
+        i, (_, pr01_err) = next(
+            (i, x) for i, x in enumerate(validate_results["errors"]) if x[0] == "PR01"
+        )
+        # numpydoc.validate always finds these vars even if they don't "exist",
+        # since they are put in by the EnumMeta Metaclass in a surprising way
+        enum_mystery_vars = ["start", "value", "names", "qualname", "module", "type"]
+        for var in enum_mystery_vars:
+            pr01_err = re.sub(f"'{var}'(, )?", "", pr01_err)
+        if "{}" in pr01_err:
+            # there are no remaining undocumented variables, remove this error
+            del validate_results["errors"][i]
+        else:
+            # there are still variables that need documenting...
+            validate_results["errors"][i] = ("PR01", pr01_err)
+
     return NumpydocReturn(name=name, **validate_results)
 
 
@@ -50,22 +70,32 @@ def _all_results(args):
         for _, modname, _ in pkgutil.walk_packages(
             [package_dir], prefix=Path(package_dir).name + "."
         ):
+            logger.info(f"Found module: {modname}.")
             if modname in args.exclude_modules:
                 continue
+            logger.info(f"Attempting to import module: {modname}.")
             mod = importlib.import_module(modname)
+            logger.info(f"Import successful: {modname}.")
             for member_name, member in inspect.getmembers(
                 mod, lambda mem: inspect.getmodule(mem) == mod
             ):
                 full_name = f"{modname}.{member_name}"
+                logger.info(f"Found module member: {full_name}.")
                 if full_name in args.exclude_members:
                     continue
-                results.append(_validate_name(full_name, args))
+                if dataclasses.is_dataclass(member) and args.ignore_dataclasses:
+                    continue
+                results.append(
+                    _validate_name(full_name, args, is_enum=type(member) is EnumMeta)
+                )
                 if inspect.isclass(member):
+                    logger.info(f"Recursively traversing class: {full_name}.")
                     for submember_name, _ in _class_submembers_r(member):
                         if submember_name in args.exclude_members:
                             continue
                         full_name = f"{modname}.{member_name}.{submember_name}"
-                        results.append(_validate_name(full_name, args))
+                        logger.info(f"Found class member: {full_name}.")
+                        results.append(_validate_name(full_name, args, is_enum=False))
     return results
 
 
@@ -104,6 +134,12 @@ def _parse_args():
         nargs="+",
         default=list(),
         help="numpydoc error codes to ignore",
+    )
+    parser.add_argument(
+        "--ignore-dataclasses",
+        type=bool,
+        default=False,
+        help="set to True if you document your dataclass fields 'inline' with Sphinx.",
     )
     return parser.parse_args()
 
